11bd57622bd93db5d995447bfe1d3fda357b4754
aiyoufu <15194170623@163.com>
2024-03-20 15:45:44 +0800

    chore: :hammer: added Vue Router and adjusted codebase for better structure and routing

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' // 导入插件
 import './style.css'
 import App from './App.vue'
+import router from './router'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate) // 在创建 Pinia 实例后使用插件
 
 const app = createApp(App)
+app.use(router)
 app.use(pinia)
 app.mount('#app')
