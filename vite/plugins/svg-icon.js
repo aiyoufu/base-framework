e525008d5be3c7d4098ed00397d84d5bbaf9a0ff
aiyoufu <15194170623@163.com>
2024-03-21 14:48:05 +0800

    feat: :sparkles: add vitest and dependencies

--- vite/plugins/svg-icon.js
+++ vite/plugins/svg-icon.js
@@ -1,11 +1,16 @@
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+// 将 import.meta.url 转换为当前文件的路径
+const __filename = fileURLToPath(import.meta.url)
+// 然后使用 path.dirname 获取目录路径
+const __dirname = path.dirname(__filename)
 
 export default function createSvgIcon(isBuild) {
   return createSvgIconsPlugin({
-    iconDirs: [
-      path.resolve(import.meta.env.VITE_PROJECT_ROOT, 'src/assets/icons/svg'),
-    ],
+    // 使用转换后的 __dirname 来解析目录路径
+    iconDirs: [path.resolve(__dirname, 'src/assets/icons/svg')],
     symbolId: 'icon-[dir]-[name]',
     svgoOptions: isBuild,
   })
